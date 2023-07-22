1337e4d6827ea8804176a46504ea11b70d7b6519
BoxunW <tony1572002@gmail.com>
2023-07-22 14:58:44 +0800

    feat: add mongoose

--- app.js
+++ app.js
@@ -1,6 +1,24 @@
 const express = require('express')
+const mongoose = require('mongoose')
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
 const app = express()
 const port = 3000
+
+// 設定連線到 mongoDB
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+
+// 取得資料庫連線狀態
+const db = mongoose.connection
+// 連線異常
+db.on('error', () => {
+  console.log('mongodb error!')
+})
+// 連線成功
+db.once('open', () => {
+  console.log('mongodb connected!')
+})
 
 // 設定首頁路由
 app.get('/', (req, res) => {
