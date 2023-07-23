ad92117df09a893e14d99f81e2efebda783dfffb
BoxunW <tony1572002@gmail.com>
2023-07-23 15:14:50 +0800

    feat: add user login page

--- routes/modules/users.js
+++ routes/modules/users.js
@@ -5,5 +5,12 @@ const router = express.Router()
 router.get('/login', (req, res) => {
   res.render('login')
 })
+router.post('/login', (req, res) => {
+})
+
+// 設定註冊路由
+router.get('/register', (req, res) => {
+  res.render('register')
+})
 
 module.exports = router
